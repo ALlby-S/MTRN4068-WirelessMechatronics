afcb26ed99b3c5e243a938ce2289e66bb4bcc04d
AlbertoSamanoHinojosa46629416 <alberto.samanohinojosa@students.mq.edu.au>
2024-08-02 13:20:44 +1000

    Added some extra code examples.
    added a loop, if statement too, some extra notes regarding functions.

--- Wk2_prac.py
+++ Wk2_prac.py
@@ -1,5 +1,6 @@
 #1. print hello world
 
+##### Functions need to be defined above the code that uses the function.
 def hello():
     #Triple quotes are used as Comments
     """Print "Hellow World" and Return None."""
@@ -9,3 +10,10 @@ for _ in range(5):
     hello()
 
 print("\n")
+
+
+#2. printing something else
+a = 1
+if a == 2:
+    print("a equals 2. \n")
+
