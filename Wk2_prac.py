f353fcf3f4f2684fb24be1063320060f565a94df
AlbertoSamanoHinojosa46629416 <alberto.samanohinojosa@students.mq.edu.au>
2024-08-02 13:35:04 +1000

    Added some extra examples of python coding.
    Defining several variables in one line, printing to console with ints and strings together.

--- Wk2_prac.py
+++ Wk2_prac.py
@@ -23,3 +23,19 @@ fnum = 5.0
 print(fnum)
 fnum2 = float(20) #Type casting.
 print(fnum2)
+
+#4. Defning strings
+##In Pyhton strings can be defined with single or double quotes
+name1 = "What's going on m8"
+name2 = 'What in the world is going on??'
+print(name1 +' '+ name2) #strings can be appended together!
+print("\n")
+
+
+#5. Defning several variables in one line
+a,b,c = 1,2,3
+
+print("a = " + str(a)) #Casting an int as a string
+print("b = " + str(b))
+print("c = " + str(c))
+
