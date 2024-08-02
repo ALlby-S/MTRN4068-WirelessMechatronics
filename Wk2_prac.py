47a767325c956d2a9fc4baaea0139b5e1e4fb6cd
AlbertoSamanoHinojosa46629416 <alberto.samanohinojosa@students.mq.edu.au>
2024-08-02 13:09:53 +1000

    Added Wk2 practice python code
    Short test code, printing to the console, loops etc.

---
+++ Wk2_prac.py
@@ -0,0 +1,11 @@
+#1. print hello world
+
+def hello():
+    #Triple quotes are used as Comments
+    """Print "Hellow World" and Return None."""
+    print("\n Hello World")
+
+for _ in range(5):
+    hello()
+
+print("\n")
