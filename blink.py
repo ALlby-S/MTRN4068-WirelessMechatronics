2e888d772743097706da4fcb99806c39fbae768d
Alberto S. <161109569+ALlby-S@users.noreply.github.com>
2024-08-08 06:27:26 +0000

    BlinkBetter
    for Ultrasonicsense range test

--- blink.py
+++ blink.py
@@ -1,12 +1,16 @@
-from gpiozero import LED
+from gpiozero import LED, Servo
 from time import sleep
 
 led = LED(17)
 
+motor = Servo(27)
+
 while True:
     led.on()
+    motor.value =0.5
     print("LED should be ON")
-    sleep(0.5) #Sleep for 1 second
+    sleep(1) #Sleep for 1 second
     led.off()
+    motor.value = 0
     print("LED should be OFF")
-    sleep(0.5)
+    sleep(1)
